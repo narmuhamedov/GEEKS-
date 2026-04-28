c08b6c1178ec33311f76f3ede6bd619e9d85180c
narmuhamedov <kingradomir@gmail.com>
2026-04-28 18:53:48 +0600

    lesson2

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('message/', views.message, name='message'),
     path('emodji/', views.emodji, name='emodji'),
+    path('blog_list/', views.blog_list_view, name='blog_list'),
+    path('blog_list/<int:id>/', views.blog_detail_view, name='blog_id'),
+    
 ]
