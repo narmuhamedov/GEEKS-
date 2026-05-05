619374b540e8859255b65cf83df53adf2c745db7
narmuhamedov <kingradomir@gmail.com>
2026-05-05 18:39:17 +0600

    cruds

--- main/settings.py
+++ main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'auto',
+    'donates',
 ]
 
 MIDDLEWARE = [
--- main/urls.py
+++ main/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('', include('auto.urls'))
+    path('', include('auto.urls')),
+    path('', include('donates.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
